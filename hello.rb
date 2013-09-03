268a0e9b40c53e75dba4d32dc178f147643fffe1
Arup Chakrabarti <arup@pagerduty.com>
2013-09-03 16:39:47 -0700

    Added basic logging to requests.log file

--- hello.rb
+++ hello.rb
@@ -1,6 +1,12 @@
 require 'rubygems'
 require 'sinatra'
 
+configure do
+  file = File.new("requests.log", 'a+')
+  use Rack::CommonLogger, file
+end
+
+
 get '/' do
   "Hello! The current time is #{Time.now.strftime('%H:%M')}"
 end
